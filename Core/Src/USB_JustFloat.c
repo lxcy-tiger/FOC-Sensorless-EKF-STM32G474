9972e8a7733f2935d0666d78d163addfa81aeb8a
lxcy <2749203562@qq.com>
2026-01-11 14:06:02 +0800

    修改发送的数据和readme.md

--- Core/Src/USB_JustFloat.c
+++ Core/Src/USB_JustFloat.c
@@ -12,7 +12,7 @@ float USB_data[USB_MaxDataRecordLength+1000];//长度为USB_MaxDataRecordLength+
 unsigned char tail[4]={0x00,0x00,0x80,0x7f};
 //记录电机运行和系统状态，用于发送给上位机
 void recordRunningData() {
-    USB_data[USB_DataRecordIndex++]=IF_startStep;
+    USB_data[USB_DataRecordIndex++]=ClarkePark.park.Ialpha_I;
     USB_data[USB_DataRecordIndex++]=ClarkePark.park.Ibeta_I;
     USB_data[USB_DataRecordIndex++]=Speed_PIstate.Set;
     USB_data[USB_DataRecordIndex++]=Speed_PIstate.Measure;
